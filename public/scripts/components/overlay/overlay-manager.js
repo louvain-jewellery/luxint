577382519fbe9964aa24824e4588aaf7e67f688d
kemanisann <louvainwebsite@gmail.com>
2025-08-15 11:31:38 +0700

    sdad

--- public/scripts/components/overlay/overlay-manager.js
+++ public/scripts/components/overlay/overlay-manager.js
@@ -7,6 +7,18 @@ export async function showOverlay(overlayName) {
     const overlay = document.querySelector(".js-overlay");
     setTimeout(() => overlay.classList.add("show"), 10);
     document.body.style.overflow = "hidden";
+
+    history.pushState({ overlayOpen: true }, "", location.href);
+
+    function handleBackButton(event) {
+      if (event.state && event.state.overlayOpen) {
+        closeOverlay();
+        window.removeEventListener("popstate", handleBackButton);
+      }
+    }
+
+    window.addEventListener("popstate", handleBackButton);
+
     return overlay;
   } catch (error) {
     throw new Error(`Failed to fetch overlay: ${error}`);
@@ -20,4 +32,8 @@ export function closeOverlay() {
   overlay.classList.remove("show");
   document.body.style.overflow = "auto";
   setTimeout(() => overlay.remove(), 300);
+
+  if (history.state && history.state.overlayOpen) {
+    history.back();
+  }
 }
