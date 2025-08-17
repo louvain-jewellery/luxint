330e92dfa6c86efc39bd1553fcaceb87c936543d
kemanisann <louvainwebsite@gmail.com>
2025-08-17 13:22:02 +0700

    dfs

--- public/scripts/components/overlay/add-item.js
+++ public/scripts/components/overlay/add-item.js
@@ -5,6 +5,7 @@ export function showAddOverlay() {
     .querySelector(".js-add-button")
     .addEventListener("click", async () => {
       const overlay = await showOverlay("add-item");
+      renderAddOverlay(overlay);
 
       overlay
         .querySelector(".js-close-button")
@@ -18,4 +19,28 @@ export function showAddOverlay() {
     });
 }
 
-function renderAddOverlay() {}
+function renderAddOverlay(overlay) {
+  const parameter = getCurrentSalesId();
+  if (!parameter) {
+    overlay.innerHTML = "";
+    const p = document.createElement("p");
+    p.classList.add("customers__warning", "warning");
+    p.textContent = "Pilih sales terlebih dahulu";
+
+    customerList.appendChild(p);
+    return;
+  }
+
+  const title = overlay.querySelector(".js-overlay-header-title");
+  title.textContent = `Tambahkan Item: ${getCurrentSalesId()}`;
+}
+
+function getCurrentSalesId() {
+  const hash = location.hash.slice(1);
+  const [pageName, parameter] = hash.split("/");
+
+  if (parameter) {
+    return parseInt(parameter);
+  }
+  return null;
+}
