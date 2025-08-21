6e1bf6cc9e5070710c1cdc2bf945f3429b927dab
kemanisann <louvainwebsite@gmail.com>
2025-08-21 12:46:16 +0700

    asds

--- public/scripts/ui/header.js
+++ public/scripts/ui/header.js
@@ -1,6 +1,8 @@
 export function loadTitle(data) {
   if (data) {
     const headerTitle = document.querySelector(".js-header-name");
+    const headerPicture = document.querySelector(".js-header-picture");
     headerTitle.textContent = data.name;
+    headerPicture.src = data.src;
   }
 }
