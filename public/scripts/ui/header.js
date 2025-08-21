f4cd906dfa25b2d3af0ff7bae26480ac95117476
kemanisann <louvainwebsite@gmail.com>
2025-08-21 12:54:58 +0700

    dsad

--- public/scripts/ui/header.js
+++ public/scripts/ui/header.js
@@ -1,8 +1,17 @@
 export function loadTitle(data) {
   if (data) {
     const headerTitle = document.querySelector(".js-header-name");
-    const headerPicture = document.querySelector(".js-header-picture");
+    const pictureWrapper = document.querySelector("js-header-picture-wrapper");
+    const headerPicture = pictureWrapper.querySelector(".js-header-picture");
     headerTitle.textContent = data.name;
-    headerPicture.src = data.image;
+    if (!headerPicture) {
+      headerPicture.src = data.image;
+    } else {
+      pictureWrapper.innerHTML = "";
+      pictureWrapper.innerHTML = `
+        <div class="customers__icon-wrapper">
+          <p class="customers__icon">${generateInitials(data.name)}</p>
+      `;
+    }
   }
 }
